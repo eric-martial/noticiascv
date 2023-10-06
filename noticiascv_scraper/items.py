146daa2d0cf2c181089c5012954586680c8c2ba6
Eric Tavares <47213032+eric-martial@users.noreply.github.com>
2023-10-06 22:27:16 +0000

    Updated the spider to crawl every topic. Scrapes article and go to next page if exist.

--- noticiascv_scraper/items.py
+++ noticiascv_scraper/items.py
@@ -2,7 +2,10 @@ import scrapy
 
 
 class ArticleItem(scrapy.Item):
+    source = scrapy.Field()
     title = scrapy.Field()
-    date = scrapy.Field()
+    author = scrapy.Field()
+    date_pub = scrapy.Field()
+    text_html = scrapy.Field()
     topic = scrapy.Field()
     link = scrapy.Field()
--- noticiascv_scraper/spiders/SantiagoMagazineSpider.py
+++ noticiascv_scraper/spiders/SantiagoMagazineSpider.py
@@ -19,24 +19,32 @@ class SantiagoMagaZineSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        highlighted_article = response.css('.col-xl-12.col-lg-6.col-md-6.col-sm-12.mb-50')
-        if highlighted_article:
-            item = ArticleItem()
-            item['title'] = highlighted_article.css('h3.title-semibold-dark a::text').get()
-            item['link'] = highlighted_article.css('h3.title-semibold-dark a::attr(href)').extract_first()
-            item['date'] = highlighted_article.css('div.post-date-dark ul li::text').getall()[-1]
-            path_segments = urlparse(response.url).path.split('/')
-            item['topic'] = path_segments[-1] if path_segments else None
-            yield item
-        
-        for article in response.css('div.row div.media.media-none--lg'):
-            item = ArticleItem()
-            item['title'] = article.css('h3.title-semibold-dark a::text').get()
-            item['link'] = article.css('h3.title-semibold-dark a::attr(href)').get()
-            item['topic'] = article.css('div.topic-box-sm::text').get()
-            item['date'] = article.css('div.post-date-dark ul li::text').get()
-            yield item
-        
+        page_urls = response.css('h3.title-semibold-dark a::attr(href)').getall()
+
+        self.logger.info(page_urls)
+
+        for page_url in page_urls:
+            page_url = response.urljoin(page_url)
+            yield scrapy.Request(url=page_url, callback=self.parse_news)
+
         next_page = response.css('div.pagination-btn-wrapper li.page-item a::attr(href)').extract()[-1]
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
+
+    def parse_news(self, response):
+        req_url = response.url
+
+        self.logger.info(f"request page: {req_url}")
+
+        item = ArticleItem()
+        item['source'] = self.name
+        article_block = response.css('div.news-details-layout1')
+        item['title'] = article_block.css('h2.title-semibold-dark::text').get()
+        article_publication = response.css('div.news-details-layout1 ul.post-info-dark>li>a::text').getall()
+        item['author'] = article_publication[-3]
+        item['date_pub'] = article_publication[-1]
+        item['link'] = req_url
+        item['topic'] = article_block.css('div.topic-box-sm::text').get()
+        item['text_html'] = article_block.css('blockquote,p').getall()
+
+        return item

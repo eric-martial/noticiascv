52d40be37540cefa12bb1e825e25718b8f6de853
Eric Martial <ericmartial.tavares@gmail.com>
2023-10-07 09:23:26 -0100

    Updated spider to format date

--- noticiascv_scraper/spiders/SantiagoMagazineSpider.py
+++ noticiascv_scraper/spiders/SantiagoMagazineSpider.py
@@ -1,6 +1,6 @@
 from ..items import ArticleItem
 import scrapy
-from urllib.parse import urlparse
+import dateparser
 
 
 
@@ -9,13 +9,13 @@ class SantiagoMagaZineSpider(scrapy.Spider):
     allowed_domains = ['santiagomagazine.cv']
     base_url = 'https://santiagomagazine.cv'
     start_urls = [
-        'https://santiagomagazine.cv/economia'#,
-        # 'https://santiagomagazine.cv/politica',
-        # 'https://santiagomagazine.cv/cultura',
-        # 'https://santiagomagazine.cv/diaspora',
-        # 'https://santiagomagazine.cv/editorial',
-        # 'https://santiagomagazine.cv/outros-mundos',
-        # 'https://santiagomagazine.cv/sociedad'
+        'https://santiagomagazine.cv/economia',
+        'https://santiagomagazine.cv/politica',
+        'https://santiagomagazine.cv/cultura',
+        'https://santiagomagazine.cv/diaspora',
+        'https://santiagomagazine.cv/editorial',
+        'https://santiagomagazine.cv/outros-mundos',
+        'https://santiagomagazine.cv/sociedad'
     ]
 
     def parse(self, response):
@@ -41,8 +41,12 @@ class SantiagoMagaZineSpider(scrapy.Spider):
         article_block = response.css('div.news-details-layout1')
         item['title'] = article_block.css('h2.title-semibold-dark::text').get()
         article_publication = response.css('div.news-details-layout1 ul.post-info-dark>li>a::text').getall()
-        item['author'] = article_publication[-3]
-        item['date_pub'] = article_publication[-1]
+        item['author'] = article_publication[-3]        
+        parsed_date = dateparser.parse(article_publication[-1])
+        if parsed_date:
+            item['date_pub'] = parsed_date.strftime('%d-%m-%Y')
+        else:
+            item['date_pub'] = article_publication[-1]
         item['link'] = req_url
         item['topic'] = article_block.css('div.topic-box-sm::text').get()
         item['text_html'] = ' <br/> '.join(article_block.css('blockquote::text,p::text').getall())

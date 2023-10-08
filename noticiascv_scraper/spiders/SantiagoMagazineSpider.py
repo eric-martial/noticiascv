2f230ee2d5ea9b051edf7675f5054196122189cb
Eric Tavares <47213032+eric-martial@users.noreply.github.com>
2023-10-08 13:14:19 +0000

    Normalized date published field.

--- noticiascv_scraper/spiders/SantiagoMagazineSpider.py
+++ noticiascv_scraper/spiders/SantiagoMagazineSpider.py
@@ -1,7 +1,7 @@
 from ..items import ArticleItem
 import scrapy
 import dateparser
-
+from datetime import datetime
 
 
 class SantiagoMagaZineSpider(scrapy.Spider):
@@ -9,14 +9,18 @@ class SantiagoMagaZineSpider(scrapy.Spider):
     allowed_domains = ['santiagomagazine.cv']
     base_url = 'https://santiagomagazine.cv'
     start_urls = [
-        'https://santiagomagazine.cv/economia',
-        'https://santiagomagazine.cv/politica',
-        'https://santiagomagazine.cv/cultura',
-        'https://santiagomagazine.cv/diaspora',
-        'https://santiagomagazine.cv/editorial',
-        'https://santiagomagazine.cv/outros-mundos',
-        'https://santiagomagazine.cv/sociedad'
+        'https://santiagomagazine.cv/economia'
+        # 'https://santiagomagazine.cv/politica',
+        # 'https://santiagomagazine.cv/cultura',
+        # 'https://santiagomagazine.cv/diaspora',
+        # 'https://santiagomagazine.cv/editorial',
+        # 'https://santiagomagazine.cv/outros-mundos',
+        # 'https://santiagomagazine.cv/sociedad'
     ]
+
+    custom_settings = {
+        'FEED_EXPORT_ENCODING': 'utf-8',
+    }
 
     def parse(self, response):
         page_urls = response.css('h3.title-semibold-dark a::attr(href)').getall()
@@ -31,6 +35,11 @@ class SantiagoMagaZineSpider(scrapy.Spider):
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
 
+    def __normalize_date(self, date_obj):
+        if not isinstance(date_obj, datetime):
+            return None
+        return date_obj.strftime('%Y-%m-%d')
+
     def parse_news(self, response):
         req_url = response.url
 
@@ -43,12 +52,10 @@ class SantiagoMagaZineSpider(scrapy.Spider):
         article_publication = response.css('div.news-details-layout1 ul.post-info-dark>li>a::text').getall()
         item['author'] = article_publication[-3]        
         parsed_date = dateparser.parse(article_publication[-1])
-        if parsed_date:
-            item['date_pub'] = parsed_date.strftime('%d-%m-%Y')
-        else:
-            item['date_pub'] = article_publication[-1]
+        item['date_pub'] = self.__normalize_date(parsed_date)
         item['link'] = req_url
         item['topic'] = article_block.css('div.topic-box-sm::text').get()
         item['text_html'] = ' <br/> '.join(article_block.css('blockquote::text,p::text').getall())
 
         return item
+

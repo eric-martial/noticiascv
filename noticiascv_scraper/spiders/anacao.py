0578a8ee7df5bbabc66f6f395c7cc1873aacccf3
Eric Tavares <47213032+eric-martial@users.noreply.github.com>
2023-10-09 22:23:23 +0000

    Parse function for anacao.cv done.

--- noticiascv_scraper/spiders/anacao.py
+++ noticiascv_scraper/spiders/anacao.py
@@ -5,9 +5,9 @@ from datetime import datetime
 
 
 class anacao(scrapy.Spider):
-    name = "santiagomagazine"
-    allowed_domains = ['santiagomagazine.cv']
-    base_url = 'https://santiagomagazine.cv'
+    name = "anacao"
+    allowed_domains = ['anacao.cv']
+    base_url = 'https://anacao.cv'
     start_urls = [
         'https://santiagomagazine.cv/economia',
         'https://santiagomagazine.cv/politica',
@@ -22,4 +22,15 @@ class anacao(scrapy.Spider):
         'FEED_EXPORT_ENCODING': 'utf-8',
     }
 
+    def parse(self, response):
+        hero_urls = response.css('div#feat-top-wrap a::attr(href)').getall()
+        page_urls = hero_urls.append(response.css('div#archive-list-wrap li>a::attr(href)').getall())
+
+        for page_url in page_urls:
+            page_url = response.urljoin(page_url)
+            yield scrapy.Request(url=page_url, callback=self.parse_news)
+
+        next_page = response.css('div.pagination a::attr(href)').getall()[-2]
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
     

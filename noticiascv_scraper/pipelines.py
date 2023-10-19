2eb2c4a002021b53bdcd94a2526c0c615d2f9455
Eric Tavares <47213032+eric-martial@users.noreply.github.com>
2023-10-19 16:29:34 +0000

    Using secret key in pipeline for supabase instead of normal key.

--- noticiascv_scraper/pipelines.py
+++ noticiascv_scraper/pipelines.py
@@ -8,17 +8,17 @@ class SaveArticlesToSupabasePipeline:
 
     def __init__(self):
         self.url: str = config.get("SUPABASE_URL")
-        self.key: str = config.get("SUPABASE_KEY")
+        self.key: str = config.get("SUPABASE_SECRET_KEY")
         self.supabase: Client = create_client(self.url, self.key)
 
     def process_item(self, item, spider):
-        data, count = self.supabase.table('articles').insert({
-                                    'source':  item['source'],
-                                    'title': item['title'],
-                                    'author':  item['author'],
-                                    'date_pub':  item['date_pub'],
-                                    'link':  item['link'],
-                                    'topic':  item['topic'],
-                                    'content':  item['text_html']
-                                }).execute()
+        self.supabase.table('articles').insert({
+            'source':  item['source'],
+            'title': item['title'],
+            'author':  item['author'],
+            'date_pub':  item['date_pub'],
+            'link':  item['link'],
+            'topic':  item['topic'],
+            'content':  item['text_html']
+        }).execute()
         return item

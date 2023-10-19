5b0a2e0e26b4556645cf9330f3773150414e2e88
Eric Tavares <47213032+eric-martial@users.noreply.github.com>
2023-10-19 18:27:39 +0000

    Initalize supabase connect in the process_item method.

--- noticiascv_scraper/pipelines.py
+++ noticiascv_scraper/pipelines.py
@@ -1,18 +1,17 @@
 from supabase import create_client, Client
 from dotenv import dotenv_values
 
-config = dotenv_values("../.env")
+config = dotenv_values('../.env')
 
 
 class SaveArticlesToSupabasePipeline:
 
-    def __init__(self):
-        self.url: str = config.get("SUPABASE_URL")
-        self.key: str = config.get("SUPABASE_SECRET_KEY")
-        self.supabase: Client = create_client(self.url, self.key)
-
     def process_item(self, item, spider):
-        self.supabase.table('articles').insert({
+        url: str = config.get("SUPABASE_URL")
+        key: str = config.get("SUPABASE_SECRET_KEY")
+        supabase: Client = create_client(url, key)
+
+        supabase.table('articles').insert({
             'source':  item['source'],
             'title': item['title'],
             'author':  item['author'],
@@ -21,4 +20,5 @@ class SaveArticlesToSupabasePipeline:
             'topic':  item['topic'],
             'content':  item['text_html']
         }).execute()
+        
         return item
